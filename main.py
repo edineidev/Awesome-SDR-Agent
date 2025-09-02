58cce504718477e87916934875ff4e8ba9622649
Edinei Cavalcanti <hi@edinei.dev>
2025-09-02 10:01:03 -0300

    Add `uv.lock` for dependency management.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from dotenv import load_dotenv
+#from openai import OpenAI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+load_dotenv()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #client = OpenAI()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # response = client.responses.create(
+    #     model="gpt-5-nano",
+    #     input="Write a one-sentence bedtime story about a unicorn."
+    # )
+    response = { "output_text": "Hello World" }
+
+    print(response["output_text"])
